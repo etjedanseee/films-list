4a627ed791a0799fde476c18725515f04625fd43
etjedanseee <etjedanseee@gmail.com>
2024-10-17 22:47:51 +0300

    add closing mediaTypeFilter on bg

--- src/components/MediaTypeFilter.tsx
+++ src/components/MediaTypeFilter.tsx
@@ -1,52 +1,81 @@
-import React, { useState } from 'react'
+import React, { MouseEvent, useEffect, useState } from 'react'
 import { MediaTypeFilterOptions } from '../types/data'
 import { mediaTypeFilterArr } from '../utils/consts'
 
 interface MediaTypeFilterProps {
-  mediaTypeFilter: MediaTypeFilterOptions,
-  onMediaTypeFilterClick: (option: MediaTypeFilterOptions) => void,
+	mediaTypeFilter: MediaTypeFilterOptions
+	onMediaTypeFilterClick: (option: MediaTypeFilterOptions) => void
 }
 
-const MediaTypeFilter = ({ mediaTypeFilter, onMediaTypeFilterClick }: MediaTypeFilterProps) => {
-  const [isOptionsListOpen, setIsOptionsListOpen] = useState(false)
+const MediaTypeFilter = ({
+	mediaTypeFilter,
+	onMediaTypeFilterClick,
+}: MediaTypeFilterProps) => {
+	const [isOptionsListOpen, setIsOptionsListOpen] = useState(false)
 
-  const handleOptionsListOpen = () => {
-    setIsOptionsListOpen(prev => !prev)
-  }
+	const handleOptionsListOpen = () => {
+		setIsOptionsListOpen((prev) => !prev)
+	}
 
-  const handleOptionClick = (option: MediaTypeFilterOptions) => {
-    onMediaTypeFilterClick(option)
-    localStorage.setItem('mediaTypeFilter', option.title)
-    handleOptionsListOpen()
-  }
+	const handleOptionClick = (option: MediaTypeFilterOptions) => {
+		onMediaTypeFilterClick(option)
+		localStorage.setItem('mediaTypeFilter', option.title)
+		handleOptionsListOpen()
+	}
 
-  return (
-    <div className='relative flex gap-x-2 items-center font-medium'>
-      <div
-        className={`select-none text-white border-white border-2 rounded-md cursor-pointer xs:py-1 py-[5px]
+	const handleOptionsClose = (e: MouseEvent) => {
+		e.stopPropagation()
+		handleOptionsListOpen()
+	}
+
+	useEffect(() => {
+		if (isOptionsListOpen) {
+			document.body.style.overflow = 'hidden'
+		} else {
+			document.body.style.overflow = 'auto'
+		}
+		return () => {
+			document.body.style.overflow = 'auto'
+		}
+	}, [isOptionsListOpen])
+
+	return (
+		<div className='relative flex gap-x-2 items-center font-medium'>
+			<div
+				className={`select-none text-white border-white border-2 rounded-md cursor-pointer xs:py-1 py-[5px]
           min-w-[100px] text-center
         `}
-        onClick={handleOptionsListOpen}
-      >
-        {mediaTypeFilter.title}
-      </div>
-      {isOptionsListOpen && (
-        <div className='absolute top-full mt-1 z-10 bg-mygray3 w-full text-center rounded-md flex flex-col gap-y-3 py-3'>
-          {mediaTypeFilterArr.map(option => (
-            <div
-              key={option.type}
-              className={`${option.type === mediaTypeFilter.type ? 'text-yellow-400' : 'text-white'} 
+				onClick={handleOptionsListOpen}
+			>
+				{mediaTypeFilter.title}
+			</div>
+			{isOptionsListOpen && (
+				<>
+					<div
+						className='fixed top-0 left-0 z-40 w-full h-full'
+						onClick={handleOptionsClose}
+					/>
+					<div className='absolute top-full mt-1 z-50 bg-mygray3 w-full text-center rounded-md flex flex-col gap-y-3 py-3'>
+						{mediaTypeFilterArr.map((option) => (
+							<div
+								key={option.type}
+								className={`${
+									option.type === mediaTypeFilter.type
+										? 'text-yellow-400'
+										: 'text-white'
+								} 
                 cursor-pointer select-none leading-none font-bold
               `}
-              onClick={() => handleOptionClick(option)}
-            >
-              {option.title}
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  )
+								onClick={() => handleOptionClick(option)}
+							>
+								{option.title}
+							</div>
+						))}
+					</div>
+				</>
+			)}
+		</div>
+	)
 }
 
 export default MediaTypeFilter
--- src/components/SitesManager.tsx
+++ src/components/SitesManager.tsx
@@ -8,182 +8,205 @@ import Button from '../UI/Button'
 import { removeHttpFromUrl } from '../utils/removeHttpsFromUrl'
 import Loader from '../UI/Loader'
 import Confirmation from '../UI/Confirmation'
-import { DragDropContext, Draggable, DropResult, Droppable } from 'react-beautiful-dnd'
+import {
+	DragDropContext,
+	Draggable,
+	DropResult,
+	Droppable,
+} from 'react-beautiful-dnd'
 
 const SitesManager = () => {
-  const { sites } = useTypedSelector(state => state.sites)
-  const { user } = useTypedSelector(state => state.auth)
-  const [loading, setLoading] = useState(false)
-  const { updateUserSites } = useActions()
-  const [isAddNewSite, setIsAddNewSite] = useState(false)
-  const [newSiteValue, setNewSiteValue] = useState('')
-  const [newSiteValueError, setNewSiteValueError] = useState('')
-  const [isConfirmDeleteSiteVisible, setIsConfirmDeleteSiteVisible] = useState(false)
-  const [siteToDelete, setSiteToDelete] = useState('')
-  const [userSites, setUserSites] = useState<string[]>([])
+	const { sites } = useTypedSelector((state) => state.sites)
+	const { user } = useTypedSelector((state) => state.auth)
+	const [loading, setLoading] = useState(false)
+	const { updateUserSites } = useActions()
+	const [isAddNewSite, setIsAddNewSite] = useState(false)
+	const [newSiteValue, setNewSiteValue] = useState('')
+	const [newSiteValueError, setNewSiteValueError] = useState('')
+	const [isConfirmDeleteSiteVisible, setIsConfirmDeleteSiteVisible] =
+		useState(false)
+	const [siteToDelete, setSiteToDelete] = useState('')
+	const [userSites, setUserSites] = useState<string[]>([])
 
-  const onUpdateSite = (prevSite: string, updatedSite: string) => {
-    if (!user) {
-      return;
-    }
-    const removedUrlUpdatedSite = removeHttpFromUrl(updatedSite)
-    const updatedSites = sites.map(s => s === prevSite ? removedUrlUpdatedSite : s)
-    updateUserSites(user.id, updatedSites, setLoading)
-  }
+	const onUpdateSite = (prevSite: string, updatedSite: string) => {
+		if (!user) {
+			return
+		}
+		const removedUrlUpdatedSite = removeHttpFromUrl(updatedSite)
+		const updatedSites = sites.map((s) =>
+			s === prevSite ? removedUrlUpdatedSite : s
+		)
+		updateUserSites(user.id, updatedSites, setLoading)
+	}
 
-  const onDeleteSiteClick = (site: string) => {
-    if (sites.length === 1) {
-      return
-    }
-    setIsConfirmDeleteSiteVisible(true)
-    setSiteToDelete(site)
-  }
+	const onDeleteSiteClick = (site: string) => {
+		if (sites.length === 1) {
+			return
+		}
+		setIsConfirmDeleteSiteVisible(true)
+		setSiteToDelete(site)
+	}
 
-  const handleCloseDeleteSiteConfirmation = () => {
-    setIsConfirmDeleteSiteVisible(false)
-    setSiteToDelete('')
-  }
+	const handleCloseDeleteSiteConfirmation = () => {
+		setIsConfirmDeleteSiteVisible(false)
+		setSiteToDelete('')
+	}
 
-  const onDeleteSite = (site: string) => {
-    handleCloseDeleteSiteConfirmation()
-    if (!user) {
-      return;
-    }
-    const updatedSites = sites.filter(s => s !== site)
-    updateUserSites(user.id, updatedSites, setLoading)
-  }
+	const onDeleteSite = (site: string) => {
+		handleCloseDeleteSiteConfirmation()
+		if (!user) {
+			return
+		}
+		const updatedSites = sites.filter((s) => s !== site)
+		updateUserSites(user.id, updatedSites, setLoading)
+	}
 
-  const onAddNewSite = (site: string) => {
-    const removedUrlUpdatedSite = removeHttpFromUrl(site)
-    if (!user || newSiteValueError || sites.includes(removedUrlUpdatedSite)) {
-      return;
-    }
-    const updatedSites = [...sites, removedUrlUpdatedSite]
-    updateUserSites(user.id, updatedSites, setLoading)
-    handleIsAddNewSite()
-  }
+	const onAddNewSite = (site: string) => {
+		const removedUrlUpdatedSite = removeHttpFromUrl(site)
+		if (!user || newSiteValueError || sites.includes(removedUrlUpdatedSite)) {
+			return
+		}
+		const updatedSites = [...sites, removedUrlUpdatedSite]
+		updateUserSites(user.id, updatedSites, setLoading)
+		handleIsAddNewSite()
+	}
 
-  const handleIsAddNewSite = () => {
-    setIsAddNewSite(prev => !prev)
-    setNewSiteValue('')
-    setNewSiteValueError('')
-  }
+	const handleIsAddNewSite = () => {
+		setIsAddNewSite((prev) => !prev)
+		setNewSiteValue('')
+		setNewSiteValueError('')
+	}
 
-  const onNewSiteValueChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value
-    const trimmedSite = value.trim()
-    if (!trimmedSite.length) {
-      setNewSiteValueError('Field is required')
-    } else {
-      setNewSiteValueError('')
-    }
-    setNewSiteValue(value)
-  }
+	const onNewSiteValueChange = (e: ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value
+		const trimmedSite = value.trim()
+		if (!trimmedSite.length) {
+			setNewSiteValueError('Field is required')
+		} else {
+			setNewSiteValueError('')
+		}
+		setNewSiteValue(value)
+	}
 
-  const onDragEnd = (result: DropResult) => {
-    const { destination, draggableId, source } = result
-    const currSite = sites.find(site => site === draggableId)
-    const destinationCondition = !destination || (destination.droppableId === source.droppableId && destination.index === source.index)
-    if (destinationCondition || !currSite || !user) {
-      return;
-    }
-    const newUserSites = Array.from(userSites)
-    const [deleted] = newUserSites.splice(source.index, 1)
-    newUserSites.splice(destination.index, 0, deleted)
-    updateUserSites(user.id, newUserSites, setLoading)
-  }
+	const onDragEnd = (result: DropResult) => {
+		const { destination, draggableId, source } = result
+		const currSite = sites.find((site) => site === draggableId)
+		const destinationCondition =
+			!destination ||
+			(destination.droppableId === source.droppableId &&
+				destination.index === source.index)
+		if (destinationCondition || !currSite || !user) {
+			return
+		}
+		const newUserSites = Array.from(userSites)
+		const [deleted] = newUserSites.splice(source.index, 1)
+		newUserSites.splice(destination.index, 0, deleted)
+		updateUserSites(user.id, newUserSites, setLoading)
+	}
 
-  useEffect(() => {
-    setUserSites(sites)
-  }, [sites])
+	useEffect(() => {
+		setUserSites(sites)
+	}, [sites])
 
-  if (loading || !userSites.length) {
-    return (
-      <div className='flex-1 flex justify-center items-center'><Loader size='80' /></div>
-    )
-  }
+	if (loading || !userSites.length) {
+		return (
+			<div className='flex-1 flex justify-center items-center'>
+				<Loader size='80' />
+			</div>
+		)
+	}
 
-  return (
-    <div className='max-w-full'>
-      <div className='text-2xl font-medium mb-3'>Your sites:</div>
-      <div className='flex flex-col gap-y-2 font-medium xs:max-w-xs mb-4'>
-        {!!userSites.length && (
-          <DragDropContext onDragEnd={onDragEnd}>
-            <Droppable droppableId='sites'>
-              {(provided, snapshot) => (
-                <div
-                  ref={provided.innerRef}
-                  {...provided.droppableProps}
-                  className={`flex flex-col gap-y-2 font-medium 
-                      ${snapshot.isDraggingOver ? 'border-myblue px-2 py-1 overflow-x-hidden border-2' : ''}
+	return (
+		<div className='max-w-full'>
+			<div className='text-2xl font-medium mb-3'>My sites:</div>
+			<div className='flex flex-col gap-y-2 font-medium xs:max-w-xs mb-4'>
+				{!!userSites.length && (
+					<DragDropContext onDragEnd={onDragEnd}>
+						<Droppable droppableId='sites'>
+							{(provided, snapshot) => (
+								<div
+									ref={provided.innerRef}
+									{...provided.droppableProps}
+									className={`flex flex-col gap-y-2 font-medium 
+                      ${
+												snapshot.isDraggingOver
+													? 'border-myblue px-2 py-1 overflow-x-hidden border-2'
+													: ''
+											}
                   `}
-                >
-                  {userSites.map((site, index) => (
-                    <Draggable
-                      draggableId={site}
-                      index={index}
-                      key={site}
-                    >
-                      {provided2 => (
-                        <SiteItem
-                          site={site}
-                          key={site}
-                          onDeleteSite={onDeleteSiteClick}
-                          onUpdateSite={onUpdateSite}
-                          dragRef={provided2.innerRef}
-                          draggableProps={provided2.draggableProps}
-                          dragHandleProps={provided2.dragHandleProps}
-                        />
-                      )}
-                    </Draggable>
-                  ))}
-                  {provided.placeholder}
-                </div>
-              )}
-            </Droppable>
-          </DragDropContext>
-        )}
-      </div>
+								>
+									{userSites.map((site, index) => (
+										<Draggable
+											draggableId={site}
+											index={index}
+											key={site}
+										>
+											{(provided2) => (
+												<SiteItem
+													site={site}
+													key={site}
+													onDeleteSite={onDeleteSiteClick}
+													onUpdateSite={onUpdateSite}
+													dragRef={provided2.innerRef}
+													draggableProps={provided2.draggableProps}
+													dragHandleProps={provided2.dragHandleProps}
+												/>
+											)}
+										</Draggable>
+									))}
+									{provided.placeholder}
+								</div>
+							)}
+						</Droppable>
+					</DragDropContext>
+				)}
+			</div>
 
-      {isAddNewSite && (
-        <div className='my-4 flex items-start gap-x-4'>
-          <Input
-            error={newSiteValueError}
-            name='Site url'
-            onInputChange={onNewSiteValueChange}
-            isFieldDirty={true}
-            placeholder='Enter site url'
-            value={newSiteValue}
-            className='flex-1'
-            py='py-2'
-          />
-          <div>
-            <Button
-              onClick={() => onAddNewSite(newSiteValue)}
-              title='Add'
-              className='py-[6px]'
-              disabled={!newSiteValue.length}
-            />
-          </div>
-        </div>
-      )}
+			{isAddNewSite && (
+				<div className='my-4 flex items-start gap-x-4'>
+					<Input
+						error={newSiteValueError}
+						name='Site url'
+						onInputChange={onNewSiteValueChange}
+						isFieldDirty={true}
+						placeholder='Enter site url'
+						value={newSiteValue}
+						className='flex-1'
+						py='py-2'
+					/>
+					<div>
+						<Button
+							onClick={() => onAddNewSite(newSiteValue)}
+							title='Add'
+							className='py-[6px]'
+							disabled={!newSiteValue.length}
+						/>
+					</div>
+				</div>
+			)}
 
-      <div
-        className='flex items-center gap-x-3 font-medium hover:cursor-pointer'
-        onClick={handleIsAddNewSite}
-      >
-        <CloseIcon className={`${isAddNewSite ? 'rotate-0' : 'rotate-45'} fill-white h-5 w-5`} />
-        <div className='text-lg'>{isAddNewSite ? 'Cancel' : 'Add new site'}</div>
-      </div>
-      {isConfirmDeleteSiteVisible && (
-        <Confirmation
-          title='Confirm delete site'
-          onClose={handleCloseDeleteSiteConfirmation}
-          onConfirm={() => onDeleteSite(siteToDelete)}
-        />
-      )}
-    </div >
-  )
+			<div
+				className='flex items-center gap-x-3 font-medium hover:cursor-pointer'
+				onClick={handleIsAddNewSite}
+			>
+				<CloseIcon
+					className={`${
+						isAddNewSite ? 'rotate-0' : 'rotate-45'
+					} fill-white h-5 w-5`}
+				/>
+				<div className='text-lg'>
+					{isAddNewSite ? 'Cancel' : 'Add new site'}
+				</div>
+			</div>
+			{isConfirmDeleteSiteVisible && (
+				<Confirmation
+					title='Confirm delete site'
+					onClose={handleCloseDeleteSiteConfirmation}
+					onConfirm={() => onDeleteSite(siteToDelete)}
+				/>
+			)}
+		</div>
+	)
 }
 
 export default SitesManager
