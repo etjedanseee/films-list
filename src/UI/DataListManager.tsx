ea70dee6fbd9925a2ae8d73b1178d3c08c1504a8
etjedanseee <etjedanseee@gmail.com>
2024-10-21 00:04:15 +0300

    show date when added to list

--- src/UI/DataListManager.tsx
+++ src/UI/DataListManager.tsx
@@ -10,171 +10,185 @@ import { useActions } from '../hooks/useActions'
 import Confirmation from './Confirmation'
 
 interface DataListManagerProps {
-  previewDataItem: IPreviewDataItem,
-  id: number | null,
-  setId: (id: number | null) => void,
-  sitesResults: ILink[],
-  isHideListsTitles?: boolean,
-  inLists: IInLists,
-  setInLists: (newInLists: IInLists) => void,
-  notes: string,
+	previewDataItem: IPreviewDataItem
+	id: number | null
+	setId: (id: number | null) => void
+	sitesResults: ILink[]
+	isHideListsTitles?: boolean
+	inLists: IInLists
+	setInLists: (newInLists: IInLists) => void
+	notes: string
 }
 
-const DataListManager = ({ id, setId, inLists, setInLists, previewDataItem, notes, sitesResults, isHideListsTitles = false }: DataListManagerProps) => {
-  const { lists } = useTypedSelector(state => state.lists)
-  const { data } = useTypedSelector(state => state.data)
-  const { user } = useTypedSelector(state => state.auth)
-  const { saveDataToSb, updateDataInListsOnSb, deleteDataOnSb } = useActions()
-  const [loading, setLoading] = useState(false)
-  const [isSaveToListsModalVisible, setIsSaveToListsModalVisible] = useState(false)
-  const [isConfirmDeleteVisible, setIsConfirmDeleteVisible] = useState(false)
-  const isDataInSavedList = Object.keys(inLists).find(key => lists.slice(3).find(a => a.id === +key))
+const DataListManager = ({
+	id,
+	setId,
+	inLists,
+	setInLists,
+	previewDataItem,
+	notes,
+	sitesResults,
+	isHideListsTitles = false,
+}: DataListManagerProps) => {
+	const { lists } = useTypedSelector((state) => state.lists)
+	const { data } = useTypedSelector((state) => state.data)
+	const { user } = useTypedSelector((state) => state.auth)
+	const { saveDataToSb, updateDataInListsOnSb, deleteDataOnSb } = useActions()
+	const [loading, setLoading] = useState(false)
+	const [isSaveToListsModalVisible, setIsSaveToListsModalVisible] =
+		useState(false)
+	const [isConfirmDeleteVisible, setIsConfirmDeleteVisible] = useState(false)
+	const isDataInSavedList = Object.keys(inLists).find((key) =>
+		lists.slice(3).find((a) => a.id === +key)
+	)
 
-  const onListClick = (e: MouseEvent, listId: number) => {
-    e.stopPropagation()
-    if (!user) {
-      return;
-    }
-    const listsEntries = Object.entries(inLists)
-    const currentList = { [listId]: new Date().toISOString() }
-    if (!listsEntries.length) {
-      const stubId = 999999
-      const itemWithLinks: IDataItemWithLinks = {
-        ...previewDataItem,
-        links: sitesResults,
-        inLists: currentList,
-        id: stubId,
-        notes,
-      }
-      setInLists(currentList)
-      saveDataToSb(itemWithLinks, user.id, setLoading, setId, data)
-    } else if (id != null) {
-      if (inLists[listId]) {
-        const filteredEntries = listsEntries.filter(entr => +entr[0] !== listId)
-        const filteredLists = Object.fromEntries(filteredEntries)
-        if (filteredEntries.length) {
-          setInLists(filteredLists)
-          updateDataInListsOnSb(id, filteredLists, setLoading, data)
-        } else {
-          setIsConfirmDeleteVisible(true)
-        }
-      } else {
-        const updatedInLists = { ...inLists, ...currentList }
-        setInLists(updatedInLists)
-        updateDataInListsOnSb(id, updatedInLists, setLoading, data)
-      }
-    }
-  }
+	const onListClick = (e: MouseEvent, listId: number) => {
+		e.stopPropagation()
+		if (!user) {
+			return
+		}
+		const listsEntries = Object.entries(inLists)
+		const currentList = { [listId]: new Date().toISOString() }
+		if (!listsEntries.length) {
+			const stubId = 999999
+			const itemWithLinks: IDataItemWithLinks = {
+				...previewDataItem,
+				links: sitesResults,
+				inLists: currentList,
+				id: stubId,
+				notes,
+			}
+			setInLists(currentList)
+			saveDataToSb(itemWithLinks, user.id, setLoading, setId, data)
+		} else if (id != null) {
+			if (inLists[listId]) {
+				const filteredEntries = listsEntries.filter(
+					(entr) => +entr[0] !== listId
+				)
+				const filteredLists = Object.fromEntries(filteredEntries)
+				if (filteredEntries.length) {
+					setInLists(filteredLists)
+					updateDataInListsOnSb(id, filteredLists, setLoading, data)
+				} else {
+					setIsConfirmDeleteVisible(true)
+				}
+			} else {
+				const updatedInLists = { ...inLists, ...currentList }
+				setInLists(updatedInLists)
+				updateDataInListsOnSb(id, updatedInLists, setLoading, data)
+			}
+		}
+	}
 
-  const handleSaveToListsModalVisible = (e: MouseEvent<HTMLDivElement>) => {
-    e.stopPropagation()
-    setIsSaveToListsModalVisible(prev => !prev)
-  }
+	const handleSaveToListsModalVisible = (e: MouseEvent<HTMLDivElement>) => {
+		e.stopPropagation()
+		setIsSaveToListsModalVisible((prev) => !prev)
+	}
 
-  const onSaveClick = (e: MouseEvent<HTMLDivElement>) => {
-    e.stopPropagation()
-    handleSaveToListsModalVisible(e)
-  }
+	const onSaveClick = (e: MouseEvent<HTMLDivElement>) => {
+		e.stopPropagation()
+		handleSaveToListsModalVisible(e)
+	}
 
-  const isDataInList = (listId: number) => {
-    return !!inLists[listId]
-  }
+	const isDataInList = (listId: number) => {
+		return !!inLists[listId]
+	}
 
-  const onDeleteData = () => {
-    if (!id) {
-      return;
-    }
-    const currId = id
-    setInLists({})
-    setId(null)
-    deleteDataOnSb(currId, setLoading, data)
-    onCloseConfirmDeleteData()
-  }
+	const onDeleteData = () => {
+		if (!id) {
+			return
+		}
+		const currId = id
+		setInLists({})
+		setId(null)
+		deleteDataOnSb(currId, setLoading, data)
+		onCloseConfirmDeleteData()
+	}
 
-  const onCloseConfirmDeleteData = () => {
-    setIsConfirmDeleteVisible(false)
-  }
+	const onCloseConfirmDeleteData = () => {
+		setIsConfirmDeleteVisible(false)
+	}
 
-  useEffect(() => {
-    const currData = data.find(item => item.dataId === previewDataItem.dataId && item.mediaType === previewDataItem.mediaType)
-    if (currData) {
-      setInLists(currData.inLists)
-      setId(currData.id)
-    }
-  }, [previewDataItem, data, setId, setInLists])
+	useEffect(() => {
+		const currData = data.find(
+			(item) =>
+				item.dataId === previewDataItem.dataId &&
+				item.mediaType === previewDataItem.mediaType
+		)
+		if (currData) {
+			setInLists(currData.inLists)
+			setId(currData.id)
+		}
+	}, [previewDataItem, data, setId, setInLists])
 
-  return (
-    <>
-      {!lists.length || loading ? (
-        <>
-          {isHideListsTitles ? (
-            <div className='py-[6px] bg-mygray3 flex justify-center items-center'>
-              <Loader size='28' />
-            </div>
-          )
-            : (
-              <div className='py-2 bg-mygray3 flex justify-center items-center'>
-                <Loader size='48' />
-              </div>
-            )
-          }
-        </>
-      )
-        : (
-          <div className='grid grid-cols-4 text-small tracking-tighter rounded-b-md bg-mygray3 select-none'>
-            <DataListManagetItem
-              isDataInList={isDataInList(lists[0].id)}
-              isHideListsTitles={isHideListsTitles}
-              list={lists[0]}
-              onListClick={onListClick}
-            />
-            <DataListManagetItem
-              isDataInList={isDataInList(lists[1].id)}
-              isHideListsTitles={isHideListsTitles}
-              list={lists[1]}
-              onListClick={onListClick}
-            />
-            <DataListManagetItem
-              isDataInList={isDataInList(lists[2].id)}
-              isHideListsTitles={isHideListsTitles}
-              list={lists[2]}
-              onListClick={onListClick}
-            />
-            <div
-              className={`flex flex-col gap-y-1 justify-between items-center ${isHideListsTitles ? 'py-1' : 'py-2'} 
+	return (
+		<>
+			{!lists.length || loading ? (
+				<>
+					{isHideListsTitles ? (
+						<div className='py-[6px] bg-mygray3 flex justify-center items-center'>
+							<Loader size='28' />
+						</div>
+					) : (
+						<div className='py-2 bg-mygray3 flex justify-center items-center'>
+							<Loader size='48' />
+						</div>
+					)}
+				</>
+			) : (
+				<div className='grid grid-cols-4 text-small tracking-tighter rounded-b-md bg-mygray3 select-none'>
+					{new Array(3).fill(0).map((_, index) => (
+						<DataListManagetItem
+							isDataInList={isDataInList(lists[index].id)}
+							isHideListsTitles={isHideListsTitles}
+							list={lists[index]}
+							onListClick={onListClick}
+							dateAddedToList={inLists[lists[index].id]}
+						/>
+					))}
+					<div
+						className={`flex flex-col gap-y-1 justify-between items-center 
               px-1 hover:cursor-pointer rounded-br-md 
+              ${isHideListsTitles ? 'py-1' : 'py-2'} 
               ${isDataInSavedList ? 'bg-yellow-500 text-black' : ''}
             `}
-              onClick={onSaveClick}
-              title={isDataInSavedList ? 'Saved' : 'Save'}
-            >
-              <BookmarkIcon className={`h-7 w-7 ${isDataInSavedList ? 'fill-mygray' : 'fill-white'}`} />
-              {!isHideListsTitles && (
-                <div className={`${isDataInSavedList ? 'text-black font-medium' : 'text-white'} font-medium`}>
-                  Save
-                </div>
-              )}
-            </div>
-          </div>
-        )
-      }
-      {isSaveToListsModalVisible && (
-        <SavedListsModal
-          handleClose={e => handleSaveToListsModalVisible(e)}
-          additionalLists={lists.slice(3)}
-          onListClick={onListClick}
-          dataInLists={inLists}
-        />
-      )}
-      {isConfirmDeleteVisible && (
-        <Confirmation
-          title='Confirm delete data'
-          onConfirm={onDeleteData}
-          onClose={onCloseConfirmDeleteData}
-        />
-      )}
-    </>
-  )
+						onClick={onSaveClick}
+						title={isDataInSavedList ? 'Saved' : 'Save'}
+					>
+						<BookmarkIcon
+							className={`h-7 w-7 ${
+								isDataInSavedList ? 'fill-mygray' : 'fill-white'
+							}`}
+						/>
+						{!isHideListsTitles && (
+							<div
+								className={`${
+									isDataInSavedList ? 'text-black font-medium' : 'text-white'
+								} font-medium`}
+							>
+								Save
+							</div>
+						)}
+					</div>
+				</div>
+			)}
+			{isSaveToListsModalVisible && (
+				<SavedListsModal
+					handleClose={(e) => handleSaveToListsModalVisible(e)}
+					additionalLists={lists.slice(3)}
+					onListClick={onListClick}
+					dataInLists={inLists}
+				/>
+			)}
+			{isConfirmDeleteVisible && (
+				<Confirmation
+					title='Confirm delete data'
+					onConfirm={onDeleteData}
+					onClose={onCloseConfirmDeleteData}
+				/>
+			)}
+		</>
+	)
 }
 
 export default DataListManager
--- src/UI/DataListManagetItem.tsx
+++ src/UI/DataListManagetItem.tsx
@@ -5,33 +5,43 @@ import { ReactComponent as PlayIcon } from '../assets/DataListManagerIcons/play.
 import { ReactComponent as SuccessIcon } from '../assets/DataListManagerIcons/success.svg'
 
 interface DataListManagetItemProps {
-  isHideListsTitles: boolean,
-  isDataInList: boolean,
-  list: IList,
-  onListClick: (e: MouseEvent<HTMLDivElement>, listId: number) => void,
+	isHideListsTitles: boolean
+	isDataInList: boolean
+	list: IList
+	dateAddedToList: string
+	onListClick: (e: MouseEvent<HTMLDivElement>, listId: number) => void
 }
 
-const DataListManagetItem = ({ isHideListsTitles, isDataInList, list, onListClick }: DataListManagetItemProps) => {
-  return (
-    <div
-      className={`flex flex-col gap-y-1 justify-between items-center ${isHideListsTitles ? 'py-1' : 'py-2'}  
-      border-r-[2px] border-zinc-900 ${isDataInList ? 'bg-myblue' : ''}
-      px-1 hover:cursor-pointer ${list.name === 'Want to watch' && 'rounded-bl-md'}`
-      }
-      onClick={(e) => onListClick(e, list.id)}
-      title={list.name}
-    >
-      {list.orderNum === 0 ?
-        <HeartIcon className='h-7 w-7 fill-white' />
-        : list.orderNum === 1 ?
-          <PlayIcon className='h-8 w-8 -mt-[2px] ml-1 fill-white' />
-          : list.orderNum === 2 ?
-            <SuccessIcon className='h-7 w-7 fill-white' />
-            : null
-      }
-      {!isHideListsTitles && <div>{list.name}</div>}
-    </div>
-  )
+const DataListManagetItem = ({
+	isHideListsTitles,
+	isDataInList,
+	list,
+	dateAddedToList,
+	onListClick,
+}: DataListManagetItemProps) => {
+	const title = isDataInList
+		? `${list.name} (Added ${dateAddedToList.slice(0, 16)})`
+		: list.name
+	return (
+		<div
+			className={`flex flex-col gap-y-1 justify-between items-center px-1 hover:cursor-pointer border-r-[2px] border-zinc-900
+        ${isHideListsTitles ? 'py-1' : 'py-2'}
+        ${isDataInList ? 'bg-myblue' : ''}
+        ${list.name === 'Want to watch' && 'rounded-bl-md'}
+      `}
+			onClick={(e) => onListClick(e, list.id)}
+			title={title}
+		>
+			{list.orderNum === 0 ? (
+				<HeartIcon className='h-7 w-7 fill-white' />
+			) : list.orderNum === 1 ? (
+				<PlayIcon className='h-8 w-8 -mt-[2px] ml-1 fill-white' />
+			) : list.orderNum === 2 ? (
+				<SuccessIcon className='h-7 w-7 fill-white' />
+			) : null}
+			{!isHideListsTitles && <div>{list.name}</div>}
+		</div>
+	)
 }
 
 export default DataListManagetItem
